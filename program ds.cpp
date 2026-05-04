6fcf60ce9f11cab24b988b60dda161f98e928705
JayanthRallapalli <rallapallijayanth@gmail.com>
2026-05-04 13:45:54 +0530

    Add files via upload

---
+++ program ds.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+int main()
+{
+    struct node *newnode;
+    struct node *temp;
+
+    newnode = (struct node*)malloc(sizeof(struct node));
+
+    newnode->data = 20;
+    newnode->next = NULL;
+
+    printf("%d", newnode->data);
+
+    temp = newnode;
+
+    return 0;
+}
